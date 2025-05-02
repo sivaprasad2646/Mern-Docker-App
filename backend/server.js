89bdc6abb31a9493a729aa8801d081ec46f27160
sivaprasad2646 <sivasiva2646@gmail.com>
2025-05-02 18:11:05 +0530

    Mern Docker App

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI || "mongodb://mongo:27017/mern_db";
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+    res.send('Hello from Backend!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
